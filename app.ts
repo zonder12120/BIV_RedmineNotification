11afd8beda0b71d6f64924ab665539f1541e8fa3
loki87by <loki87.666@gmail.com>
2023-12-08 10:55:11 +0300

    kill any type && add env

--- app.ts
+++ app.ts
@@ -1,72 +1,90 @@
-import TelegramBot from 'node-telegram-bot-api';
-import axios, {AxiosResponse} from 'axios';
+import TelegramBot from "node-telegram-bot-api";
+import axios, { AxiosResponse } from "axios";
+import { Issue, IssueContent } from "./types";
 
-const telegramBotToken = '6709631142:AAHWiY1GSFaT_P2-dxCuWFQpBxCrZrpbO1w';
-const chatId = '-1002064116476';
-const redmineUrl = 'https://redmine.bivgroup.com/projects/sberinssupport';
-const redmineApiKey = '73174af619ca4ba2f9481d46269dfb1111926d1e';
-const bot = new TelegramBot(telegramBotToken, {polling: false});
-const request = `${redmineUrl}/issues.json?key=${redmineApiKey}&status_id=*&limit=500`;
+const { TELEGRAM_BOT_TOKEN, CHAT_ID, REDMINE_URL, REDMINE_API_KEY } =
+  process.env;
 
-let currentIssuesList: any[] = [];
+const bot = new TelegramBot(TELEGRAM_BOT_TOKEN as string, { polling: false });
+const request = `${REDMINE_URL}/issues.json?key=${REDMINE_API_KEY}&status_id=*&limit=500`;
+
+let currentIssuesList: Issue[] = [];
 
 async function initializeCurrentIssuesList(): Promise<void> {
-    try {
-        const response: AxiosResponse = await axios.get(request);
-        currentIssuesList = response.data.issues;
-        console.log(currentIssuesList);
-    } catch (error) {
-        console.error('Ошибка при инициализации списка задач из Redmine:', error);
-    }
+  try {
+    const response: AxiosResponse = await axios.get(request);
+    currentIssuesList = response.data.issues;
+    console.log(currentIssuesList);
+  } catch (error) {
+    console.error("Ошибка при инициализации списка задач из Redmine:", error);
+  }
 }
 
 async function getRedmineUpdatesAndNotify(): Promise<void> {
-    try {
-        const response: AxiosResponse = await axios.get(request);
-        const newIssuesList = response.data.issues;
-        console.log('---------------------------------------------------\n' +
-                    '--------------------Новый массив-------------------\n' +
-                    '---------------------------------------------------\n')
-        console.log(newIssuesList);
+  try {
+    const response: AxiosResponse = await axios.get(request);
+    const newIssuesList = response.data.issues;
+    console.log(
+      "---------------------------------------------------\n" +
+        "--------------------Новый массив-------------------\n" +
+        "---------------------------------------------------\n"
+    );
+    console.log(newIssuesList);
 
-        if (JSON.stringify(currentIssuesList) !== JSON.stringify(newIssuesList)) {
-            // Обнаружены изменения
-            newIssuesList.forEach((issue: any) => {
-                if (!currentIssuesList.find((currentIssue) => currentIssue.id === issue.id)) {
-                    notifyNewIssue(issue);
-                } else {
-                    const currentIssue = currentIssuesList.find((currentIssue) => currentIssue.id === issue.id);
-                    if (currentIssue.status.name !== issue.status.name) {
-                        notifyStatusUpdate(issue, currentIssue.status.name);
-                    } else if (currentIssue.updated_on !== issue.updated_on) {
-                        notifyIssueUpdate(issue);
-                    }
-                }
-            });
-            currentIssuesList = newIssuesList;
+    if (JSON.stringify(currentIssuesList) !== JSON.stringify(newIssuesList)) {
+      // Обнаружены изменения
+      newIssuesList.forEach((issue: Issue) => {
+        if (
+          !currentIssuesList.find(
+            (currentIssue) => currentIssue.id === issue.id
+          )
+        ) {
+          notifyNewIssue(issue);
+        } else {
+          const currentIssue = currentIssuesList.find(
+            (currentIssue) => currentIssue.id === issue.id
+          );
+          if (
+            ((currentIssue as Issue).status as unknown as IssueContent).name !==
+            ((issue as Issue).status as unknown as IssueContent).name
+          ) {
+            notifyStatusUpdate(
+              issue,
+              ((currentIssue as Issue).status as unknown as IssueContent).name
+            );
+          } else if ((currentIssue as Issue).updated_on !== issue.updated_on) {
+            notifyIssueUpdate(issue);
+          }
         }
-    } catch (error) {
-        console.error('Ошибка при получении обновлений из Redmine:', error);
+      });
+      currentIssuesList = newIssuesList;
     }
+  } catch (error) {
+    console.error("Ошибка при получении обновлений из Redmine:", error);
+  }
 }
 
-function notifyNewIssue(issue: any): void {
-    const message: string = `Добавлена новая задача #${issue.id} - ${issue.subject}\n${redmineUrl}/issues/${issue.id}`;
-    bot.sendMessage(chatId, message);
+function notifyNewIssue(issue: Issue): void {
+  const message: string = `Добавлена новая задача #${issue.id} - ${issue.subject}\n${REDMINE_URL}/issues/${issue.id}`;
+  bot.sendMessage(CHAT_ID as string, message);
 }
 
-function notifyStatusUpdate(issue: any, oldStatus: string): void {
-    const message: string = `В задаче #${issue.id} изиенён статус с: "${oldStatus}" на "${issue.status.name}"\n${redmineUrl}/issues/${issue.id}`;
-    bot.sendMessage(chatId, message);
+function notifyStatusUpdate(issue: Issue, oldStatus: string): void {
+  const message: string = `В задаче #${
+    issue.id
+  } изменён статус с: "${oldStatus}" на "${
+    (issue.status as unknown as IssueContent).name
+  }"\n${REDMINE_URL}/issues/${issue.id}`;
+  bot.sendMessage(CHAT_ID as string, message);
 }
 
-function notifyIssueUpdate(issue: any): void {
-    const message: string = `Обновление в задаче #${issue.id}\n${redmineUrl}/issues/${issue.id}`;
-    bot.sendMessage(chatId, message);
+function notifyIssueUpdate(issue: Issue): void {
+  const message: string = `Обновление в задаче #${issue.id}\n${REDMINE_URL}/issues/${issue.id}`;
+  bot.sendMessage(CHAT_ID as string, message);
 }
 
 initializeCurrentIssuesList().then(() => {
-    setInterval(getRedmineUpdatesAndNotify, 60000);
-    console.log('Бот запущен. Ожидание обновлений из Redmine.');
-    bot.sendMessage(chatId, 'Бот успешно запущен и готов к работе!');
+  setInterval(getRedmineUpdatesAndNotify, 60000);
+  console.log("Бот запущен. Ожидание обновлений из Redmine.");
+  bot.sendMessage(CHAT_ID as string, "Бот успешно запущен и готов к работе!");
 });
